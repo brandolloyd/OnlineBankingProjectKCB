85e34ad7b151b172e0e02568bc64621e5de52fa8
Christal Cain <chrostalniac@gmail.com>
2025-12-03 01:28:55 -0500

    Added initial AdminController class with dependency injection setup

--- src/main/java/controller/AdminController.java
+++ src/main/java/controller/AdminController.java
@@ -1,4 +1,20 @@
 package controller;
+import entity.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import repository.UserRepository;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/admin")
 
 public class AdminController {
+    private final UserRepository userRepository;
+
+    //Constructor injection
+    public AdminController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
